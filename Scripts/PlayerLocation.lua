8fcb716466e179dbf1943ffc7bd99996b7092c9a
drpsyko101 <drpsyko101@gmail.com>
2025-05-10 19:09:41 +0800

    Fix unable to get all player location

--- Scripts/PlayerLocation.lua
+++ Scripts/PlayerLocation.lua
@@ -3,22 +3,17 @@ local UEHelpers = require("UEHelpers")
 local lastLocations = {}
 
 local function ReadPlayerLocations()
-  local gameStateBase = UEHelpers:GetGameStateBase()
-  local allPlayerStates = gameStateBase.PlayerArray
-  LogMsg(string.format("%i player state(s) found", #allPlayerStates))
-  for i = 1, #allPlayerStates, 1 do
-    local playerState = allPlayerStates[i]
-    if playerState:IsValid() and playerState.PawnPrivate:IsValid() then
+  local playerStates = UEHelpers:GetAllPlayerStates()
+  LogMsg(string.format("%i player state(s) found", #playerStates))
+  for i = 1, #playerStates, 1 do
+    local playerState = playerStates[i] ---@cast playerState AMotorTownPlayerState
+    if playerState:IsValid() then
       local playerName = playerState:GetPlayerName()
-      local playerController = playerState:GetPlayerController()
-      local pawn = playerController.Pawn
-      if pawn:IsValid() then
-        local location = pawn:K2_GetActorLocation()
-        LogMsg(string.format("Player %s location: {X=%.3f, Y=%.3f, Z=%.3f}", playerName:ToString(),
-          location.X,
-          location.Y, location.Z))
-        lastLocations[playerName] = location
-      end
+      local location = playerState.Location
+      LogMsg(string.format("PlayerPawn %s location: {X=%.3f, Y=%.3f, Z=%.3f}", playerName:ToString(),
+        location.X,
+        location.Y, location.Z))
+      lastLocations[playerName] = location
     end
   end
 end
