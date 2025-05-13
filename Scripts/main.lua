b6a4a79a4c0d767e6706a8b1e587f5bc5c63a777
drpsyko101 <drpsyko101@gmail.com>
2025-05-13 15:28:25 +0800

    Add simple webserver

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,6 +1,7 @@
 local UEHelpers = require("UEHelpers")
-
 local modName = "MotorTownMods"
+
+Webserver = nil ---@class Webserver
 
 require("PlayerLocation")
 require("ServerAutoFps")
@@ -17,4 +18,10 @@ function LogMsg(message, severity)
   print(string.format("[%s] %s: %s\n", modName, severity, message))
 end
 
+local function LoadWebserver()
+  Webserver = require("Webserver")
+  Webserver.run("*", 8080)
+end
+
+pcall(LoadWebserver)
 LogMsg("Mod loaded")
